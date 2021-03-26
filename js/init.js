3ae148bb976038e4148577e20c1806f11774f750
Alanypm <81442841+Alanypm@users.noreply.github.com>
2021-03-26 14:35:03 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,10 @@
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyANoZwHUZzfGfsx6YNXJquxlKxY7pUyXe8",
+    authDomain: "aypmi-ot.firebaseapp.com",
+    databaseURL: "https://aypmi-ot-default-rtdb.firebaseio.com",
+    projectId: "aypmi-ot",
+    storageBucket: "aypmi-ot.appspot.com",
+    messagingSenderId: "520354501811",
+    appId: "1:520354501811:web:dd38b70475d69acd30ccc6",
+});
